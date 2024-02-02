d4e7cda0fdefd765463d575b3d9e6c58e8523cfe
b166erobot <bernard0freitas@outlook.com>
2024-02-02 19:42:29 -0300

    Requisito 5 satisfeito

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ const mainDiv = document.createElement('div');
 const mainH1 = document.createElement('h1');
 const colorPaletteDiv = document.createElement('div');
 const pageDiv = document.createElement('div');
+const buttonClear = document.createElement('button');
 
 // configurar elementos.
 mainH1.innerText = 'Paleta de Cores';
@@ -17,12 +18,15 @@ pageDiv.id = 'pixel-board';
 pageDiv.style.display = 'grid';
 pageDiv.style.gridTemplateColumns = 'repeat(5, 40px)';
 pageDiv.style.gridTemplateRows = 'repeat(5, 40px)';
+buttonClear.id = 'clear-board';
+buttonClear.innerText = 'Limpar';
 
 // conectando elementos.
 body.appendChild(main);
 main.appendChild(mainDiv);
 mainDiv.appendChild(mainH1);
 mainDiv.appendChild(colorPaletteDiv);
+mainDiv.appendChild(buttonClear);
 mainDiv.appendChild(pageDiv);
 
 // criando elementos com laço for aqui.
@@ -54,7 +58,7 @@ function selectColor(event) {
   event.target.classList.add('selected');
 }
 
-for (let indexDiv = 0; indexDiv < colorPaletteDiv.children.length; indexDiv++) {
+for (let indexDiv = 0; indexDiv < colorPaletteDiv.children.length; indexDiv += 1) {
   const div = colorPaletteDiv.children[indexDiv];
   div.addEventListener('click', selectColor);
 }
@@ -68,3 +72,12 @@ for (let indexDiv = 0; indexDiv < pageDiv.children.length; indexDiv += 1) {
   const div = pageDiv.children[indexDiv];
   div.addEventListener('click', colorize);
 }
+
+function clearAllSquares() {
+  for (let indexDiv = 0; indexDiv < pageDiv.children.length; indexDiv++) {
+    const div = pageDiv.children[indexDiv];
+    div.style.backgroundColor = 'white';
+  }
+}
+
+buttonClear.addEventListener('click', clearAllSquares);
